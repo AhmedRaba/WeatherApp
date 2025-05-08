fcf32ba949661b60261177a7900e6a9010d76b34
Ahmed Raba <ahmedrapa91@gmail.com>
2025-05-09 02:13:56 +0300

    add weather conditions icons and use day/night background

--- app/src/main/java/com/instabug/weather/presentation/weather_forecast/ForecastScreen.kt
+++ app/src/main/java/com/instabug/weather/presentation/weather_forecast/ForecastScreen.kt
@@ -1,16 +1,21 @@
 package com.instabug.weather.presentation.weather_forecast
 
-import android.text.style.ForegroundColorSpan
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -23,13 +28,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.instabug.weather.R
 import com.instabug.weather.domain.model.WeatherData
-import com.instabug.weather.ui.theme.WeatherTheme
+import com.instabug.weather.presentation.current_weather.getIconResId
 import com.instabug.weather.utils.LocationProvider
 import com.instabug.weather.utils.Resource
+import java.util.Calendar
 
 @Composable
 fun ForecastScreen(viewModel: ForecastViewModel = ForecastViewModel(context = LocalContext.current)) {
@@ -39,6 +51,9 @@ fun ForecastScreen(viewModel: ForecastViewModel = ForecastViewModel(context = Lo
     val cityName = remember { mutableStateOf<String?>(null) }
     var lastLatLng by remember { mutableStateOf<Pair<Double, Double>?>(null) }
 
+    val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+    val isDaytime = currentHour in 6..18
+    val backgroundDrawable = if (isDaytime) R.drawable.iv_day else R.drawable.iv_night
 
     LaunchedEffect(Unit) {
         val location = LocationProvider.getLastKnownLocation(context)
@@ -54,15 +69,19 @@ fun ForecastScreen(viewModel: ForecastViewModel = ForecastViewModel(context = Lo
     }
 
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
     ) {
+
+        Image(
+            painter = painterResource(id = backgroundDrawable),
+            contentDescription = "Background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
         when (state) {
             Resource.Loading -> {
                 Log.d("ForecastScreen", "Showing Loading Indicator")
-
                 CircularProgressIndicator()
             }
 
@@ -70,43 +89,75 @@ fun ForecastScreen(viewModel: ForecastViewModel = ForecastViewModel(context = Lo
                 Log.d("ForecastScreen", "Showing Success Indicator")
 
                 val forecastData = (state as Resource.Success<List<WeatherData>>).data
-                LazyColumn(modifier = Modifier.fillMaxSize()) {
-                    item {
-                        cityName.value?.let {
-                            Text(
-                                text = "Weather Forecast for $it",
-                                style = MaterialTheme.typography.titleLarge,
-                                modifier = Modifier.padding(8.dp)
-                            )
-                        }
-                    }
-                    items(forecastData) { day ->
-                        Column(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(8.dp),
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
+                LazyRow(
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 30.dp)
+                ) {
 
-                            Text("Date: ${day.date}", style = MaterialTheme.typography.titleLarge)
-                            Spacer(Modifier.height(8.dp))
-                            Text(
-                                "Temp: ${day.temperature}°",
-                                style = MaterialTheme.typography.bodyLarge
-                            )
-                            Text(
-                                "Max: ${day.tempMax}°",
-                                style = MaterialTheme.typography.bodyMedium
-                            )
-                            Text(
-                                "Min: ${day.tempMin}°",
-                                style = MaterialTheme.typography.bodyMedium
-                            )
-                            Text(day.conditions, style = MaterialTheme.typography.bodyMedium)
+                    items(forecastData) { day ->
+                        val iconResId = getIconResId(context, day.icon)
+
+                        Card(
+                            modifier = Modifier.width(200.dp).padding(end = 16.dp)
+                        ) {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier.padding(16.dp)
+                            ) {
+
+                                if (iconResId != 0) {
+                                    Image(
+                                        painter = painterResource(id = iconResId),
+                                        contentDescription = day.conditions,
+                                        modifier = Modifier
+                                            .size(120.dp)
+                                            .padding(bottom = 8.dp)
+                                    )
+                                } else {
+                                    Text(
+                                        text = "⚠️ Icon not found for '${day.icon}'",
+                                        color = MaterialTheme.colorScheme.error,
+                                    )
+                                }
+
+                                cityName.value?.let {
+                                    Text(
+                                        text = it,
+                                        style = MaterialTheme.typography.bodyLarge,
+                                        color = Color.White,
+                                        fontWeight = FontWeight.Normal,
+                                        fontSize = 30.sp
+                                    )
+                                }
+
+                                Text(
+                                    "${day.temperature.toInt()}°",
+                                    style = MaterialTheme.typography.titleLarge,
+                                    fontSize = 70.sp,
+                                    color = Color.White,
+                                    fontWeight = FontWeight.Normal
+                                )
+
+                                Text(
+                                    "${day.tempMax.toInt()}°/${day.tempMin.toInt()}°",
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    fontSize = 30.sp,
+                                    color = Color.White,
+                                    fontWeight = FontWeight.Normal
+                                )
+
+                                Spacer(Modifier.height(8.dp))
+
+                                Text(
+                                    day.conditions,
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    fontSize = 26.sp,
+                                    color = Color.White,
+                                    fontWeight = FontWeight.Normal
+                                )
+                            }
                         }
                     }
                 }
-
             }
 
             is Resource.Error -> {
@@ -137,8 +188,8 @@ fun ForecastScreen(viewModel: ForecastViewModel = ForecastViewModel(context = Lo
 
 @Preview
 @Composable
-private fun Preview(){
+private fun Preview() {
     MaterialTheme {
-    ForecastScreen()
+        ForecastScreen()
     }
 }
